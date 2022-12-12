1f8d62c7e469ad100de78e34d4406c462b5dff3e
unknown <sk8695451>
2022-12-13 01:55:30 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,32 @@
 import React, {useState} from 'react'
 import '../styles/App.css';
 const App = () => {
+  const [data1, setData1] = useState('OFF')
+  const [data2, setData2] = useState('ON')
 //code here 
+  const handleClick = ()=>{
+  if (event.target.id == 'btn1'){
+  setData1('ON');
+    setData2('OFF');
+    
+  }
+    else{
+    setData1('OFF');
+    setData2('ON');
+    }
+  }
   
 
   
   return (
     <div className="App">
       <button id="btn1" onClick={handleClick}>
-      {/* assign value for button 1 */}
+      {data1}
       </button>
       <br />
       <br />
       <button id="btn2" onClick={handleClick}>
-        {/* assign value for button 2 */}
+        {data2}
       </button>
     </div>
   );
